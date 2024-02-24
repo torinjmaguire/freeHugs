5c0467db1cd838b82943504e187c1d4af1a2483c
Torin Maguire <torinmaguire@gmail.com>
2024-02-24 01:24:44 -0600

    Reorganize warehouse 1 nodes into floors

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -5,7 +5,6 @@ const SPEED = 20.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
