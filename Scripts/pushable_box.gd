5819130173acf07217c130cca888843a5c07a984
Torin Maguire <torinmaguire@gmail.com>
2024-08-13 21:20:28 -0500

    Add physicsbody3d detection to push cubes

--- Scripts/pushable_box.gd
+++ Scripts/pushable_box.gd
@@ -1,21 +1,28 @@
 extends CharacterBody3D
 
 @export var push_distance := 2
-var in_push_range := false
-var player: CharacterBody3D
+@export var player: CharacterBody3D
 @export var pushable_area: Area3D
+var push_range := 3
 
 func _init() -> void:
-	# Use global position as our default position
+	# Use global position as our default position for simpler movement calculations
 	self.top_level = true
 
+func is_dest_clear(origin: Vector3, dest: Vector3) -> bool:
+	var results := get_world_3d().direct_space_state.intersect_ray(PhysicsRayQueryParameters3D.create(origin, dest))
+	
+	if not results:
+		return true
+	else:
+		return false
+
 func is_move_in_bounds(point: Vector3, area: Area3D) -> bool:
-	var space_state: PhysicsDirectSpaceState3D = get_world_3d().direct_space_state
-	var params: PhysicsPointQueryParameters3D = PhysicsPointQueryParameters3D.new()
+	var params := PhysicsPointQueryParameters3D.new()
 	params.position = point
 	params.collide_with_areas = true
 	params.collide_with_bodies = false
-	var results: Array = space_state.intersect_point(params)
+	var results := get_world_3d().direct_space_state.intersect_point(params)
 	
 	# Check every area our point is in to see if our bounding area is one of them
 	if results.any(func(dict: Dictionary) -> bool: return dict.rid == area.get_rid()):
@@ -23,15 +30,13 @@ func is_move_in_bounds(point: Vector3, area: Area3D) -> bool:
 	else:
 		return false
 
-func _on_area_3d_body_entered(body:Node3D) -> void:
-	if body.get_name() == "Player":
-		player = body
-		in_push_range = true
+func is_in_push_range() -> bool:
+	var distance := self.position.distance_to(player.global_position)
+	if distance < push_range:
+		return true
+	else:
+		return false
 
-func _on_area_3d_body_exited(body:Node3D) -> void:
-	if body.get_name() == "Player":
-		in_push_range = false
-		
 func push_box() -> void:
 	var box_position := position
 	var player_position := player.global_position
@@ -46,13 +51,13 @@ func push_box() -> void:
 	
 	# Adding 1 to account for the width of the cubes
 	direction = direction.normalized() * (push_distance + 1) * -1
-	var destination: Vector3 = direction + position
+	var destination: Vector3 = direction + box_position
 	
-	if is_move_in_bounds(destination, pushable_area):
+	if is_move_in_bounds(destination, pushable_area) and is_in_push_range() and is_dest_clear(box_position, destination):
 		var tween: Tween = create_tween()
 		tween.tween_property(self, "position", destination, 0.3)
 
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("interact") and in_push_range:
+	if Input.is_action_just_pressed("interact"):
 		push_box()
 
